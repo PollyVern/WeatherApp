7f443f01114bedd556249f0e9f9c72b7ccbf49cc
Polina Belovodskaya <polinadeveloper@gmail.com>
2023-02-17 16:48:45 +0300

    [feature/Preparation-for-work]: Add modules

---
+++ WeatherApp/Screens/Weather screen/Model/WeatherModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherModel.swift
+//  WeatherApp
+//
+//  Created by Polina Belovodskaya on 17.02.2023.
+//
+
+import Foundation
---
+++ WeatherApp/Screens/Weather screen/Presenter/WeatherPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherPresenter.swift
+//  WeatherApp
+//
+//  Created by Polina Belovodskaya on 17.02.2023.
+//
+
+import Foundation
