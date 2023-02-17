e8c366af4ad6bb70acf9d536acd0651f06699265
Polina Belovodskaya <polinadeveloper@gmail.com>
2023-02-17 12:47:53 +0300

    [feature/Preparation-for-work]: Remove SceneDelegate and add main VC

---
+++ WeatherApp/CitiesListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  CitiesListViewController.swift
+//  WeatherApp
+//
+//  Created by Polina Belovodskaya on 17.02.2023.
+//
+
+import UIKit
+
+class CitiesListViewController: UIViewController {
+
+}
